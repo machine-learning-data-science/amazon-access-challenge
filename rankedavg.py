7186a6311f2dda2aee03386d563350cc4fcf35b2
Rohan Nagar <rohan.nagar@utexas.edu>
2016-05-05 23:21:23 -0500

    Some XGBoost, and ensembling works with rankedavg.py

---
+++ rankedavg.py
@@ -0,0 +1,45 @@
+import sys
+
+from collections import defaultdict
+
+def rank_avg(files, outfile):
+    with open(outfile, 'w') as out:
+        ranks = defaultdict(list)
+        for i, f in enumerate(files):
+            file_ranks = []
+
+            lines = open(f).readlines()
+            lines = [lines[0]] + sorted(lines[1:])
+            for e, line in enumerate(lines):
+                if e > 0:
+                    r = line.strip().split(",")
+                    file_ranks.append((float(r[1]), e, r[0]))
+
+            for rank, item in enumerate(sorted(file_ranks)):
+                ranks[(item[1],item[2])].append(rank)
+
+        average_ranks = []
+        for k in sorted(ranks):
+            average_ranks.append((sum(ranks[k])/len(ranks[k]), k))
+
+        ranked_ranks = []
+        for rank, k in enumerate(sorted(average_ranks)):
+            ranked_ranks.append((k[1][0], k[1][1], rank/(len(average_ranks) - 1)))
+
+        out.write('Id,Action\n')
+        for k in sorted(ranked_ranks):
+            out.write('{},{}\n'.format(k[1], k[2]))
+
+def main():
+    if len(sys.argv) != 2:
+        print('Usage: python rankavg.py OUTFILE\nDo not include the \'.csv\' extension.')
+        exit(1)
+
+    # These files are the submissions that we are throwing together in the ensemble.
+    files = ['ReferenceCode/starter_submission.csv', 'output/xgboost_best_model.csv', 'output/rf_2ktrees_entropy_auto_boostrapped.csv']
+    
+    outfile = 'output/{}.csv'.format(sys.argv[1])
+    rank_avg(files, outfile)
+
+if __name__ == '__main__':
+    main()
